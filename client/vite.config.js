a15a6f429a13b2c15e526b0f7d29a29374d9f7db
Ayush Khandelwal <121782836+Ayush1-00@users.noreply.github.com>
2025-08-04 19:50:42 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: './', // 👈 ADD THIS LINE
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
